b3bd7eebc7b6c41bffbcf4b8fa1b4136385b3ab6
brisingr8 <almumo93@gmail.com>
2016-04-07 09:59:29 +0200

    Added files via upload

---
+++ AsignacionVariablesMuñozMolinaAlvaro.java
@@ -0,0 +1,19 @@
+/**
+10. Crea una clase AsignacionVariables con el siguiente código. 
+Corrígela si es necesario y coméntala en el mismo código fuente, indicando errores y valores de variables.
+*/
+
+class AsignacionVariables{
+	public static void main(String[] args){
+		
+		int i=2;
+		
+		// Debemos inicializar la variable, si no existiria un error.
+		
+		byte b=(byte)i;
+		
+		System.out.println(i);
+		
+		System.out.println(b);
+	}
+}
